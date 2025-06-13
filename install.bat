838f80d7e941dfdee0a26388afff2b94529b1e79
Mai Hữu Hùng <maihuuhung0205@gmail.com>
2025-06-13 11:32:56 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Installing required Python packages...
-pip install Flask Flask-SocketIO Flask-Bcrypt pyngrok werkzeug
+pip install Flask Flask-SocketIO Flask-Bcrypt pyngrok werkzeug requests
 echo Installation complete.
 pause
